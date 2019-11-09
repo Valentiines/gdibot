1fbb3f70f1bfd635f032cf3a5fb53e5b32d3041b
Liam Caust <42659474+Valentiines@users.noreply.github.com>
2019-11-10 00:12:33 +0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,4 +26,4 @@ client.on('message', message => {
 
 // THIS  MUST  BE  THIS  WAY
 
-client.login(process.env.BOT_TOKEN);//BOT_TOKEN is the Client Secret
+client.login(process.env.xCc1IBq3nZ4iUzZQ5NNGwAOzJWxJSQVW);//BOT_TOKEN is the Client Secret
